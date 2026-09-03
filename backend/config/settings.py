be7a37c17ab0ff1df4c8c2949c15c9fee96a920f
Mazene <mmazenezerguine@gmail.com>
2026-09-03 02:27:56 +0200

    promethus & grafana config + hardend build images

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,9 +45,11 @@ if DEBUG:
     INSTALLED_APPS += ["django_extensions"]
 
 MIDDLEWARE = [
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
